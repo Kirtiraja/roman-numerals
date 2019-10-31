9401ba62826e7556573a1307b410501e498e62b5
Student <example@example.com>
2019-10-31 15:08:21 -0700

    add split function to user string

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
 $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
-// var userNumberArray = result.split("")
-
-// function stringSplit(string) {
-//   return result.split(" ").forEach(function(string) {
-//     userNumberArray.push(stringSplit(string));
-
-
 
 var userNumberArray = [];
 
-
 var result = $("#userNumber").val();
  $("p#result").text(result);
-result.split("");
-userNumberArray.push(result);
-console.log(userNumberArray);
+
+
+userNumberArray.push(result.split(""));
+
+
+
+  console.log(userNumberArray);
 
 });
 });
