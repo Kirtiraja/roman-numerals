9e1e4bb6c73215de79af369d91850cfe6e653f7c
Student <example@example.com>
2019-10-31 11:45:51 -0700

    add variables to scripts.still WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
+var userNumberArray = result.split();
 
 
 
 var result = $("#userNumber").val();
  $("p#result").text(result);
 
+
+ console.log(userNumberArray);
 });
 });
