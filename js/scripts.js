2b89814911a2be6ddc06f545f4d43607c5bd8d34
Student <example@example.com>
2019-10-31 16:43:37 -0700

    add non-working code to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,26 @@ $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
 
+
 var userNumberArray = [];
+
 
 var result = $("#userNumber").val();
  $("p#result").text(result);
-
-
 userNumberArray.push(result.split(""));
 
+var romanizeNumber = function romanize(number) {
+  var userNumber = [1, 5, 10, 50, 100, 500, 1,000];
 
+  if (userNumberArray.includes(userNumber[0])) {
+  return "I" ; }
+}
 
   console.log(userNumberArray);
+  console.log(romanizeNumber)
+});
+});
 
-});
-});
+
+// var romanizeNumber = userNumberArray.map(function(number){
+//   return number + "I";
