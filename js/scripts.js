0247a766e472cc4fea80cfbbaf909928ca2ee056
Student <example@example.com>
2019-10-31 14:25:08 -0700

    add arraysplit and push to console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
-var userNumberArray = result.split();
+// var userNumberArray = result.split("")
 
+// function stringSplit(string) {
+//   return result.split(" ").forEach(function(string) {
+//     userNumberArray.push(stringSplit(string));
+
+
+
+var userNumberArray = [];
 
 
 var result = $("#userNumber").val();
  $("p#result").text(result);
+result.split("");
+userNumberArray.push(result);
+console.log(userNumberArray);
 
-
- console.log(userNumberArray);
 });
 });
